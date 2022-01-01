a198c8a602e3ac5d2bddb0cc8c96a3d4adf1b528
BaoChengyi <bcygswspring168@gmail.com>
2022-01-02 04:43:59 +0800

    render和关键字的含义

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
+// 要渲染的组建App,render声明；要控制的区域，使用连写的方式：$mount('#app'),因为index.html中，id='app',
+// 所以此处用id选择器 #app表示
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
