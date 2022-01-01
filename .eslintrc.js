00873eb0c21324b45aade54c3f36fa33ae968d1d
BaoChengyi <bcygswspring168@gmail.com>
2022-01-02 01:45:54 +0800

    vue 2.x基本概念和知识点回顾

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,22 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // 不检查单双引号
+    singleQuote: 0,
+    // 设置函数局末必须有分号，否则报错
+    semi: ['error', 'always'],
+    // 一行结束不能有空格，关闭
+    'no-trailing-spaces': 'off',
+    // 关闭空格报错检查
+    'no-irregular-whitespace': 'off',
+    // 函数名称和函数体左括号前有空格，关闭，0等同于off
+    'space-before-function-paren': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
