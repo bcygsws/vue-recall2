9374e55e55fd9f57f688a5ee9d7fccd6cad564a9
BaoChengyi <bcygswspring168@gmail.com>
2022-01-13 03:37:50 +0800

    子组件向父组件传值综述

--- src/components/pass_value/Father.vue
+++ src/components/pass_value/Father.vue
@@ -2,7 +2,7 @@
   <div class="pass_container">
     <h3>父子元素传值演示:</h3>
     <h4>{{ msg }}</h4>
-    <b-son :rec="info"></b-son>
+    <b-son :rec="info" rv="hello world" :fru="fru"></b-son>
     <h4>向子组件CSon传值</h4>
     <c-son :rev="info"></c-son>
   </div>
@@ -13,7 +13,11 @@ import BSon from './SonB.vue';
 export default {
   name: 'Father',
   data() {
-    return { msg: '向子组件BSon传值', info: '父元素向子元素传值' };
+    return {
+      msg: '向子组件BSon传值',
+      info: '父元素向子元素传值',
+      fru: ['苹果', '香蕉', '橘子', '菠萝'],
+    };
   },
   methods: {},
   components: {
--- src/components/pass_value/SonB.vue
+++ src/components/pass_value/SonB.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="b_container">
     <h3>
-      BSon子组件接收到父组件的传值：<u>{{ rec }}</u>
+      BSon子组件接收到父组件的传值：<u>{{ rec }}</u
+      ><i>{{ rv }}</i>
     </h3>
+    <!-- 父组件Father传递的eat值 -->
+    <ul>
+      <li v-for="(item, index) in fru" :key="index">{{ item }}</li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
   name: 'BSon',
-  props: ['rec'],
+  // props: ['rec', 'rv', 'fru'],
+  props: {
+    rec: {
+      type: String, // 传递值的类型
+      default: '默认值', // 默认值
+      required: true, // 表示父级必须传入数据，否则会报错
+      validate: (value) => {
+        // 校验规则
+        return value;
+      },
+    },
+    rv: { type: String },
+    fru: { type: Array },
+  },
 };
 </script>
 <style lang="less" scoped>
