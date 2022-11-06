c7cc9de07271f89551560f943f623771e9c6c7f1
bcygsws <bcygswspring168@gmail.com>
2022-11-07 03:54:15 +0800

    :art: 正则表达式

--- src/views/User.vue
+++ src/views/User.vue
@@ -65,6 +65,7 @@ export default {
     let validPass = (rule, value, callback) => {
       // 密码必须为:1.数字和字母的组合，既不能是纯数字也不能是纯字母2.长度为3到16位
       // ?!表示负前瞻，以为前面能是，https://blog.csdn.net/csm0912/article/details/81206848
+      // 正则表达式的探究：https://www.jianshu.com/p/56d589ea48a9
       const regExp = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9a-zA-Z]{3,16}$/;
       // let regExp = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{3,16}$/;
       if (value === '') {
@@ -150,6 +151,13 @@ export default {
     },
   },
 };
+/**
+ * 
+ * @ 正则表达式的用法
+ * 
+ * 
+ * 
+ */ 
 </script>
 <style lang="less" scoped>
 .login_container {
