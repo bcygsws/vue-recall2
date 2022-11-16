8bf020f1c7ea7b3e820179c6a12b1cf8b496b0da
bcygsws <bcygswspring168@gmail.com>
2022-11-17 02:48:17 +0800

    :sparkles: 富于表现力、动态的、健壮的css,stylus

--- src/components/stylus_jade/UseNew.vue
+++ src/components/stylus_jade/UseNew.vue
@@ -8,6 +8,10 @@
 /**
  *
  * @ stylus的使用
+ * stylus:富于表现力、动态、健壮的CSS
+ * stylus中文网：https://www.stylus-lang.cn/docs/interpolation.html
+ * 
+ * [ˈstaɪləs]触笔，唱针，指示笔（在电脑上书写、画画等）
  * 1.使用vue ui安装开发依赖，stylus  stylus-loader
  * 2.在.vue文件中style标签配置lang="stylus"
  *
@@ -28,7 +32,8 @@
  *
  * 版本兼容问题：
  * 安装stylus-loader stylus老是报UnhandledPromiseRejectionWarning: TypeError: loaderContext.getResolve
- *  is not a function和Module build failed: TypeError: this.getOptions is not a function的原因，那是因为stylus和stylus-loader的版本兼容问题
+ *  is not a function和Module build failed: TypeError: this.getOptions is not a function的原因，那是因
+ * 为stylus和stylus-loader的版本兼容问题
  * 
  * 原文链接：https://blog.csdn.net/small_hello/article/details/117451914
  * 参考：https://www.cnblogs.com/shy1766IT/p/11070642.html
@@ -42,6 +47,10 @@ export default {
 };
 </script>
 <style lang="stylus" scoped>
+  /* 类似python式缩进，来区分级别 */
 .un_container h3
-  color green
+   color green
+   /* &：表示使用父级选择器，后面跟一个伪类 :hover */
+   &:hover 
+     color greenyellow
 </style>
