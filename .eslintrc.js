b4f6debb36a29572ff4e289e5520bf93d254a9b1
bcygsws <bcygswspring168@gmail.com>
2022-12-01 02:32:16 +0800

    :memo: esli校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,12 @@ module.exports = {
   },
   rules: {
     /*
-    * 引号类型,为单引号；不是单引号，会给出警告；
-    * 'off'或0，关闭规则；
-    * 'warn'或1，打开规则的作为警告，不影响代码退出；
-    * 'error'或2,把规则作为一个错误(退出代码触发时为1)
-    *
-    * */
+     * 引号类型,为单引号；不是单引号，会给出警告；
+     * 'off'或0，关闭规则；
+     * 'warn'或1，打开规则的作为警告，不影响代码退出；
+     * 'error'或2,把规则作为一个错误(退出代码触发时为1)
+     *
+     * */
     // quotes: [1, 'single'],
     // 设置函数局末必须有分号，否则报错。要注意顺序，结果集是一个数组'error'在前，'always'在后
     semi: ['error', 'always'],
@@ -25,7 +25,7 @@ module.exports = {
     // 函数名称和函数体左括号前有空格，关闭，0等同于off
     'space-before-function-paren': 0,
     // "no-constant-condition": 2,// 禁止在条件中使用常量表达式 if(true) if(1)
-    "no-constant-condition": 0,// 在条件中使用常量表达式 if(true) if(1)
+    'no-constant-condition': 0, // 关闭约束条件，在条件中使用常量表达式 if(true) if(1)
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
