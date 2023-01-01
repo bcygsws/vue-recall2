c8f311315912c4eacef3523775d7d34e604f1168
bcygsws <bcygswspring168@gmail.com>
2023-01-02 05:35:12 +0800

    :ambulance: keep-ali路对应的组件的缓存

--- src/components/keep/Cache.vue
+++ src/components/keep/Cache.vue
@@ -7,9 +7,29 @@
 </template>
 <script>
 export default {
-  name: 'CacheC',
+  name: '_Cache',
   data() {
     return {};
+  },
+  // beforeRouteEnter(to, from, next) {
+  //   // ...
+  // },
+  mounted() {
+    console.log('缓存组件中的mounted阶段');
+  },
+  activated() {
+    console.log('缓存组件中的activated阶段');
+  },
+  // beforeRouteLeave(to, from, next) {
+  //   // ...
+  // },
+  deactivated() {
+    console.log('缓存组件中的deactivated阶段');
+  },
+  beforeDestroy() {
+    console.log(
+      'beforeDestroy和destroyed阶段都不会执行，因为keep-alive缓存的组件仍然在内存中'
+    );
   }
 };
 </script>
