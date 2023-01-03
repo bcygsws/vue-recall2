a8d4faaec946878c2db5e886203cece0832cb9df
bcygsws <bcygswspring168@gmail.com>
2023-01-04 05:07:08 +0800

    :sparkles: keep-alive深度分析

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from '../views/Home.vue';
 import SubComp from '@/components/sub/SubComp.vue';
 import NeedImp from '@/components/webpack/NeedImp.vue';
 import Keep from '@/components/keep/KeepAlive.vue';
-import _Cache from '@/components/keep/Cache.vue';
+import MyCache from '@/components/keep/Cache.vue';
 import NoCache from '@/components/keep/NoCache.vue';
 
 Vue.use(VueRouter);
@@ -21,11 +21,11 @@ const routes = [
     children: [
       {
         path: '/home/sub',
-        component: SubComp,
+        component: SubComp
       },
       {
         path: '/home/need',
-        component: NeedImp,
+        component: NeedImp
       },
       {
         path: '/home/keep_alive',
@@ -33,16 +33,16 @@ const routes = [
         children: [
           {
             path: '/home/keep_alive/cache',
-            component: _Cache, // 需要缓存的视图组件Cache,isAlive值为true
-            meta: { isAlive: true },
+            component: MyCache, // 需要缓存的视图组件Cache,isAlive值为true
+            meta: { isAlive: true }
           },
           {
             path: '/home/keep_alive/no_cache',
-            component: NoCache, // 需要缓存的视图组件Cache,isAlive值为true
-          },
-        ],
-      },
-    ],
+            component: NoCache // 需要缓存的视图组件Cache,isAlive值为true
+          }
+        ]
+      }
+    ]
   },
   {
     path: '/about',
@@ -51,14 +51,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
 ];
 
 const router = new VueRouter({
   routes,
   // 当前选中的路由；不声明linkExactActiveClass属性，这个值默认为："router-link-exact-active".
-  linkExactActiveClass: 'myClass',
+  linkExactActiveClass: 'myClass'
 });
 
 export default router;
